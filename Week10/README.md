e30db4c0705f0a0525a9c74bc912a3cb4711b026
郝百春 <18635724353@163.com>
2021-02-07 22:38:02 +0800

    总结

--- Week10/README.md
+++ Week10/README.md
@@ -1,1 +1,25 @@
-学习笔记
+# 浏览器的渲染流程
+ * URL--HTTP-->HTML--parse-->DOM--css computing-->DOM with css--layout--> DOM with position--render-->Bitmap
+
+# 排版/布局（layout）
+ * 根据浏览器的属性进行排版
+ * 三代排版技术
+ ```
+ 正常流: position, display, float, ...
+ Flex
+ Grid
+ ```
+ 
+## 收集元素进行
+ * 根据主轴尺寸，进行元素分行 若设置了'no-wrap' 就需要分到新的一行
+
+## 主轴的计算
+ * 计算主轴方向 *　找出所有Flex元素 *　把轴方向的剩余尺寸按比例分配 *　剩余尺寸<0，所有flex元素为0，等比例压缩剩余元素
+
+## 交叉轴的计算
+ * 根据每一行中最大元素尺寸计算行高
+ * 根据行高flex-align 和 item-align，确定元素具体位置
+
+##渲染
+ * 绘制单个元素
+ * 绘制DOM树
--- Week10/server.js
+++ Week10/server.js
@@ -1,4 +1,36 @@
 const http = require('http');
+
+const HTML = `<html maaa=a>
+<head> 
+<style>
+body{
+	background-color: rgb(0,0,0);
+}
+#box{
+width:700px;
+height:300px;
+display:flex;
+background-color: rgb(255,255,255);
+}
+#box #myid{
+width:400px;
+height:100px;
+background-color: rgb(255,0,0);
+}
+#box .c1{
+flex:1;
+background-color: rgb(0,255,0);
+}
+</style>
+</head>
+<body>
+<div id="box">
+<div id="myid" > </div>
+<div class="c1" > </div>
+</div>
+</body>
+</html>`
+
 http.createServer((request, response) => {
 	let body = [];
 	request.on("error", (err) => {
@@ -10,34 +42,7 @@ http.createServer((request, response) => {
 		body = Buffer.concat(body).toString();
 		console.log("body", body);
 		response.writeHead(200, { 'Content-Type': 'text/html' });
-		response.end(
-			`<html maaa=a>
-<head> 
-	<style>
-#box{
-	width:500px;
-	height:300px;
-	display:flex;
-	background-color: rgb(255,255,255);
-}
-#box #myid{
-	width:200px;
-	height:100px;
-	background-color: rgb(255,0,0);
-}
-#box .c1{
-	flex:1;
-	background-color: rgb(0,255,0);
-}
-	</style>
-</head>
-<body>
-	<div id="box">
-		<div id="myid" > </div>
-		<div class="c1" > </div>
-	</div>
-</body>
-</html>`);
+		response.end(HTML);
 	})
 }).listen(8088);
 
